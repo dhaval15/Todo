f4113b154573f028c81aca5f0223a576eee0fbc6
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-06-25 19:36:28 +0530

    Passed Sembast database,store through tree.

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 import 'src/app.dart';
+import 'package:sembast/sembast.dart';
+import 'package:sembast/sembast_io.dart';
 
-void main() {
-  runApp(TodoApp());
+void main() async {
+  final dir = await getApplicationDocumentsDirectory();
+  Database database = await databaseFactoryIo
+      .openDatabase(join(dir.path, 'todos.db'), version: 1);
+  StoreRef<String, dynamic> todoStore = stringMapStoreFactory.store('todos');
+  runApp(TodoApp(
+    database: database,
+    storeRef: todoStore,
+  ));
 }
-
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_utils/arch.dart';
+import 'package:flutter_utils/database.dart';
+import 'states.dart';
 import 'ui.dart' as UI;
 
 class TodoApp extends StatelessWidget {
+  final Database database;
+  final StoreRef storeRef;
+
+  const TodoApp({Key key, this.database, this.storeRef}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      initialRoute: '/splash',
-      routes: {
-        '/splash': (context) => FunctionalWidget(
-              init: UI.initSplash,
-              build: UI.buildSplash,
-            ),
-        '/home': (context) => FunctionalWidget(
-              build: UI.buildHome,
+    return Provider(
+      providers: {
+        HomeState: () => HomeState(
+              client: database,
+              todoStore: storeRef,
+              todos: Map(),
             ),
       },
-      debugShowCheckedModeBanner: false,
+      child: MaterialApp(
+        theme: ThemeData(primarySwatch: Colors.red),
+        initialRoute: '/splash',
+        routes: {
+          '/splash': (context) => FunctionalWidget(
+                init: UI.initSplash,
+                build: UI.buildSplash,
+              ),
+          '/home': (context) => FunctionalWidget(
+                build: UI.buildHome,
+              ),
+        },
+        debugShowCheckedModeBanner: false,
+      ),
     );
   }
 }
