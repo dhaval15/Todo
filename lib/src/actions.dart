fdacf3ddfd91ccb0f771e883b12c3c3f17c5faa4
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-06-25 19:19:21 +0530

    Added Actions: update[Todo], delete[Todo]

--- lib/src/actions.dart
+++ lib/src/actions.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'models.dart';
 import 'states.dart';
 import 'package:flutter_utils/database.dart' as Db;
 
@@ -15,4 +16,14 @@ FutureOr<EditTodoState> save(EditTodoState state) async {
         await Db.insert(state.client, state.todoStore, state.todo.toJson());
     return state.copyWith(todo: state.todo.copyWith(key: key));
   }
+}
+
+FutureOr<HomeState> delete(Todo todo, HomeState state) async {
+  await Db.delete(state.client, state.todoStore, todo.key);
+  return state.copyWith(todos: state.todos..remove(todo.key));
+}
+
+FutureOr<HomeState> update(Todo todo, HomeState state) async {
+  await Db.update(state.client, state.todoStore, todo.toJson());
+  return state.copyWith(todos: state.todos..[todo.key] = todo);
 }
