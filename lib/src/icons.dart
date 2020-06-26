68d16bc54da72f987cb2a6f9aaaf15c860101551
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-06-26 15:26:41 +0530

    Added Icons Temporery [Will Change in future]

---
+++ lib/src/icons.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+const IconData todo = Icons.work;
+const IconData add = Icons.add;
+const IconData edit = Icons.edit;
+const IconData home = Icons.home;
+const IconData running = Icons.help;
+const IconData completed = Icons.adb;
