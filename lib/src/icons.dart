32daa871d061615acd6854f1ad9048e536ccd643
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-06-28 13:20:47 +0530

    Added completionAvatar Icon

--- lib/src/icons.dart
+++ lib/src/icons.dart
@@ -6,3 +6,4 @@ const IconData edit = Icons.edit;
 const IconData home = Icons.home;
 const IconData running = Icons.help;
 const IconData completed = Icons.adb;
+const IconData completedAvatar = Icons.done;
