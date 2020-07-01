c96b5126a4d13dd61312cc243e35eebfb472cbc8
Dhaval Patel <dhavalpatel32768@gmail.com>
2020-07-01 23:40:56 +0530

    Updated Icons with new dependency flutter_icons

--- lib/src/icons.dart
+++ lib/src/icons.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_icons/flutter_icons.dart';
 
-const IconData todo = Icons.work;
+const IconData todo = FlutterIcons.tasks_faw;
 const IconData add = Icons.add;
 const IconData edit = Icons.edit;
 const IconData home = Icons.home;
-const IconData running = Icons.help;
-const IconData completed = Icons.adb;
+const IconData running = FlutterIcons.heartbeat_faw5s;
+const IconData completed = FlutterIcons.done_mdi;
 const IconData completedAvatar = Icons.done;
